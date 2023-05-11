57d657a98b86635ff7a4956eb89660c97ea7fc46
David Quiroga <97131802+David-Quiroga@users.noreply.github.com>
2023-05-11 18:57:15 -0500

    Delete modulos directory

--- modulos/dist/objeto.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const persona_1 = require("./persona");
-let persona = new persona_1.Persona("Luis", 12312421);
-console.log(persona.presentarPersona);
--- modulos/src/objeto.ts
+++
@@ -1,4 +0,0 @@
-import {Persona} from "./persona"
-
-let persona = new Persona("Luis", 12312421)
-console.log(persona.presentarPersona)
